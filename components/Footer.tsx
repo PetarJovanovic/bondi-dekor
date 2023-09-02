2c0f1d4a6404e84a0058d5671022041b6bf4f26f
Petar Jovanovic <pecazajecar@gmail.com>
2023-09-02 23:32:01 +0200

    Footer fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,23 +4,23 @@ import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineYout
 const Footer = () => {
   return (
     <footer className=''>
-      <div className='py-4 px-4 max-w-[1240px] mx-auto flex justify-between'>
-        <div className='text-neutral-500'>© 2023 Bondi Dekor</div>
-        <div className='text-neutral-500 flex flex-col items-start'>
-          <div>
+      <div className='py-4 px-4 max-w-[1240px] mx-auto flex flex-col justify-between gap-5 lg:flex-row lg:items-start'>
+        <div className='text-neutral-500 text-center'>© 2023 Bondi Dekor</div>
+        <div className='text-neutral-500 flex flex-col justify-center items-center'>
+          <div className='text-center'>
             Kontakt
-            <hr className='w-20 h-[1px] my-2 bg-neutral-500 border-0 rounded'></hr>
+            <hr className='w-20 h-[1px] my-2 mx-auto bg-neutral-500 border-0 rounded'></hr>
           </div>
-          <div>
+          <div className='text-center'>
             <p>Adresa: Dalmatinska 19, Paraćin</p>
             <p>E-mail: bondidekor@gmail.com</p>
             <p>Telefon: +381 (60) 55 71 104</p>
           </div>
         </div>
-        <div className='text-neutral-500 flex flex-col items-start'>
-          <div className=''>
+        <div className='text-neutral-500 flex flex-col justify-center items-center'>
+          <div className='text-center'>
             Socijalne Mreže
-            <hr className='w-20 h-[1px] my-2 bg-neutral-500 border-0 rounded'></hr>
+            <hr className='w-20 h-[1px] my-2 mx-auto bg-neutral-500 border-0 rounded'></hr>
           </div>
           <div className='flex flex-row items-center justify-center space-x-2 mb-1'>
             <a href='https://www.facebook.com/molersko.knauferski?mibextid=avESrC' rel='noreferrer' target='_blank'>
