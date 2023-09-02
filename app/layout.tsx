3b10b18e32cfbf55c5e55c8a35ca14a922c2fc79
Petar Jovanovic <pecazajecar@gmail.com>
2023-09-02 19:36:36 +0200

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,14 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import { ThemeProvider } from 'next-themes';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <body>
-        <ThemeProvider attribute='class' enableSystem={true}>
-          <Navbar />
-          {children}
-          <Footer />
-        </ThemeProvider>
+        <Navbar />
+        {children}
+        <Footer />
       </body>
     </html>
   );
